15d09a9b02a506a55c10f063212a703a9b58f207
Manasa H K <manasahk736@gmail.com>
2026-05-30 23:48:09 +0530

    feat: implement debate API and initialize new debate transcript record

--- api/main.py
+++ api/main.py
@@ -24,15 +24,7 @@ app = FastAPI(title="DebateFlow API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
